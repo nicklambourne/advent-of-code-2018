8901aa405b3b67612d8b192cecf2f0af331ae159
nicklambourne <ndl93@live.com>
2018-12-08 21:01:34 +1000

    day 2

--- day2.rb
+++ day2.rb
@@ -19,3 +19,24 @@ File.open("input/2-1.txt").each do |line|
     end
 end
 puts twos * threes
+
+
+
+
+require 'set'
+
+ids = Array.new
+File.open("input/2-1.txt").each do |line|
+     ids.push(line.strip)
+end
+for i in 0..ids[0].length
+    s = Set.new
+    ids.each do |id|
+        masked = id[0, i] + "_" + id[i+1, id.length-i]
+	if s.include?(masked)
+	    puts(id[0, i] + id[i+1, id.length-i])
+            exit
+        end
+        s.add(masked)
+    end
+end
